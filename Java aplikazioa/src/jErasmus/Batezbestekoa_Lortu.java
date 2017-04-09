b1d7031917a8dcec021b232ebf04e1d6e1c7448d
Bcolly <obeaskoetxea@gmail.com>
2017-04-09 20:35:36 +0200

    batazbestkoa lortu eginda

--- Java aplikazioa/src/jErasmus/Batezbestekoa_Lortu.java
+++ Java aplikazioa/src/jErasmus/Batezbestekoa_Lortu.java
@@ -13,64 +13,41 @@ public class Batezbestekoa_Lortu {
 
 	public static void main(String[] args) throws XQException
 	{
-		String a_doc = "input/biblio.xml";
+		String a_doc = "input/letonia.xml";
+		
+	    if (args.length == 1) {
+	    	a_doc = args[0];
+	    } 
+	    
+	    System.out.println("The input file: "+a_doc);
+	    System.out.println("");
+	    
+	    // create the data source and expression to process
+		
+	    XQDataSource xqs = new SaxonXQDataSource(); 
+	    XQConnection conn = xqs.getConnection();
+	    XQExpression xqe = conn.createExpression();
+	    
+	    xqe.bindString(new QName("doc"), a_doc, null);
+	    
+	    String xqueryString = "declare namespace era = 'www.ehu.eus/erasmus';"
+	    + "declare variable $doc external;"
+	    + "let $books := (doc($doc)//era:nota)"
+	    + "for $b in $books return xs:float($b/text())";
+	    	    
+	    XQResultSequence rs = xqe.executeQuery(xqueryString);
+	    
+	    float i = 0;
+	    int kont = 0;
+	    
+	    while (rs.next()){
+	    	i = i + rs.getFloat();
+	    	kont++;
+	    }
 
-		String fileName = "output/listofbooks.html";
-
-		if (args.length == 1) {
-			a_doc = args[0];
-		} 
-
-		System.out.println("The input file: "+a_doc);
-		System.out.println("");
-
-		// create the data source and expression to process
-
-		XQDataSource xqs = new SaxonXQDataSource(); 
-		XQConnection conn = xqs.getConnection();
-		XQExpression xqe = conn.createExpression();
-
-		// Bind variable to expression
-
-		xqe.bindString(new QName("adoc"), a_doc, null);
-
-		fileName = "output/listofbooks-"+a_doc.replace(".", "-").replace("/", "-")+".html";
-
-
-		String xqueryString = 
-				"declare variable $adoc external;\n"+
-				"let $books := doc($adoc)//book\n"+    		
-				"\n"+
-				"return <html><h1>List of Books from {$adoc}</h1>\n"+
-				"	<ul>\n"+
-				"		{for $b in $books\n"+
-				"			return <li>{$b/title/text()}</li>} "+
-				"	</ul>\n"+
-				"</html>\n";
-
-		// print the result of the query
-		try {
-
-			BufferedWriter bufferedWriter =	new BufferedWriter(new FileWriter(fileName));
-
-			XQResultSequence rs = xqe.executeQuery(xqueryString);
-
-			while(rs.next()){
-				String read = rs.getItemAsString(null);
-				System.out.println(read);
-				bufferedWriter.write(read);
-			}
-
-			bufferedWriter.close();
-		}
-		catch(IOException ex) {
-			System.out.println(
-					"Error writing to file '"
-							+ fileName + "'");
-		}
-
-		System.out.println("Finished!!");
-		conn.close();
+	    float batazb = i/kont;
+	    System.out.println("noten batazbestekoa: "+String.valueOf(batazb));
+	    conn.close();
 	}
 	
 }//Eraldatu
